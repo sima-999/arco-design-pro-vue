afb247182b2696b86f18be856d537c0b07ed5478
lieangnide <lieangnide@outlook.com>
2023-08-06 13:13:42 +0000

    fixed

--- arco-design-pro-vite/src/main.ts
+++ arco-design-pro-vite/src/main.ts
@@ -11,6 +11,7 @@ import App from './App.vue';
 // Styles are imported via arco-plugin. See config/plugin/arcoStyleImport.ts in the directory for details
 // 样式通过 arco-plugin 插件导入。详见目录文件 config/plugin/arcoStyleImport.ts
 // https://arco.design/docs/designlab/use-theme-package
+//
 import '@/assets/style/global.less';
 import '@/api/interceptor';
 
